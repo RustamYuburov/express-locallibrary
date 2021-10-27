ec8ba48f78e5a38c177f065de6d64550551990c9
Rustam Yuburov <rt138328@gmail.com>
2021-10-27 17:56:37 +0600

    Updated models files for project

--- models/author.js
+++ models/author.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const { DateTime } = require('luxon');
 
 const AuthorSchema = new Schema({
   first_name: { type: String, required: true, minLength: 2, maxLength: 100 },
@@ -15,13 +16,15 @@ AuthorSchema.virtual('name').get(function () {
 
 // Virtual for author's lifespan
 AuthorSchema.virtual('lifespan').get(function () {
-  let lifetime_string = '';
+  let lifetime_string = 'No Info';
   if (this.date_of_birth) {
     lifetime_string = DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED);
   }
   lifetime_string += ' - ';
   if (this.date_of_death) {
     lifetime_string += DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED);
+  } else {
+    lifetime_string += 'Still Alive';
   }
   return lifetime_string;
 });
